6429f511f9c587b9b46615cb4269aba94e047e75
seunghyeonbang <bangsh0718@gamil.com>
2020-06-26 02:29:48 -0500

    scorpio model plugin code clean-up

--- src/scorpio_plugin.cpp
+++ src/scorpio_plugin.cpp
@@ -1,47 +1,109 @@
-#include <functional>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
-#include <ignition/math/Vector3.hh>
-//#include "gazebo/physics/dart/DARTPhysics.hh"
-//#include "gazebo/physics/dart/DARTModel.hh"
-//#include <dart/dart.hpp>
-//#include <dart/gui/osg/osg.hpp>
-//#include <dart/utils/urdf/urdf.hpp>
-//#include <dart/utils/utils.hpp>
+#include <boost/bind.hpp>
+#include <ros/ros.h>
+
+#include <Eigen/Dense>
+
+#include <PnC/ScorpioPnC/ScorpioInterface.hpp>
+#include <Utils/IO/IOUtilities.hpp>
 
 namespace gazebo
 {
     class ScorpioPlugin : public ModelPlugin
     {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
-        {
-            // Store the pointer to the model
-            this->model = _parent;
+        public: 
+            ScorpioPlugin(){
+                total_num_joint_idx_ = Eigen::VectorXd::Zero(11);
+                active_joint_idx_ = Eigen::VectorXd::Zero(7);
+                q_ = Eigen::VectorXd::Zero(11);
+                qdot_ = Eigen::VectorXd::Zero(11);
 
-            // Listen to the update event. This event is broadcast every
-            // simulation iteration.
-            this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-                    std::bind(&ScorpioPlugin::OnUpdate, this));
+                interface_ = new ScorpioInterface();
+                sensordata_ = new ScorpioSensorData();
+                command_ = new ScorpioCommand();
+            }
 
-            //physics::DARTModel *m = (physics::DARTModel *)(model.get());
-            //dart::utils::DartLoader urdfLoade
-            //dart::dynamics::SkeletonPtr robot = urdfLoader.parseSkeleton("/home/will/workspace/PnC/RobotModel/Robot/Draco/DracoSim_Dart.urdf");
-            //m->DARTSkeleton() = robot;
-        }
+            ~ScorpioPlugin(){
+                delete interface_;
+                delete sensordata_;
+                delete command_;
+            }
 
-        // Called by the world update start event
-    public: void OnUpdate()
-        {
-            // Apply a small linear velocity to the model.
-            //this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
-        }
+            void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+            {
+                this->model = _parent; //Store the pointer to the model
 
-        // Pointer to the model
-    private: physics::ModelPtr model;
+                this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+                        std::bind(&ScorpioPlugin::OnUpdate, this)); // Listen to the update event
+                                                                    // This event is broadcast every simulation iteration
+                std::cout << "==================================================" << std::endl;
+                std::cout << "The scorpio_plugin is attached to model[" << this->model->GetName()
+                    << "]"<< std::endl;
 
-        // Pointer to the update event connection
-    private: event::ConnectionPtr updateConnection;
+                physics::Joint_V joints = this->model->GetJoints();
+                physics::Link_V links = this->model->GetLinks();
+
+                //std::cout << "====================" << std::endl;
+                //std::cout << "joint name" << std::endl;
+                //for (int i = 0; i < joints.size(); ++i) {
+                   //std::cout << joints[i]->GetName() << std::endl;
+                //}
+
+                //std::cout << "====================" << std::endl;
+                //std::cout << "link name" << std::endl;
+                //for (int i = 0; i < links.size(); ++i) {
+                   //std::cout << links[i]->GetName() << std::endl;
+                //}
+                //std::cout << "====================" << std::endl;
+
+                //Closed loop
+                total_num_joint_idx_ << 1,2,3,4,5,6,7,8,9,10,11; 
+                active_joint_idx_ << 1,2,5,6,9,10,11;
+
+
+            }
+            // Called by the world update start event
+            void OnUpdate() {
+
+                //double seconds_per_step = this ->model->Getworld()->GetPhysicsEngine()->GetUpdatePeriod();
+                //double alternate_time = this->model->Getworld()->GetSimTime().Double();
+                //iters = this->model->GetWorld()->GetIterations();
+
+                gazebo::physics::Joint_V joints = this->model->GetJoints();
+
+                for (int i = 0; i < total_num_joint_idx_.size(); ++i) {
+                    q_[i] = joints[total_num_joint_idx_[i]]->Position(0);
+                    qdot_[i] = joints[total_num_joint_idx_[i]] ->GetVelocity(0);
+                }
+                //myUtils::pretty_print(q_, std::cout, "joint pos");
+                //myUtils::pretty_print(qdot_, std::cout, "joint vel");
+
+                sensordata_->q = q_;
+                sensordata_->qdot = qdot_;
+                interface_ -> getCommand(sensordata_,command_); 
+
+                for (int i = 0; i < active_joint_idx_.size(); ++i) {
+                   joints[active_joint_idx_[i]]->SetForce(0,command_->jtrq[i]); 
+                   //joints[active_joint_idx_[i]]->SetForce(0,-qdot_[i]); 
+                   //joints[active_joint_idx_[i]]->SetForce(0,0); 
+                }
+            }
+
+
+        private: 
+            physics::ModelPtr model; //pointer to the model
+            event::ConnectionPtr updateConnection; //Pointer to the update event connection
+
+            Eigen::VectorXd active_joint_idx_;
+            Eigen::VectorXd total_num_joint_idx_;
+            Eigen::VectorXd q_;
+            Eigen::VectorXd qdot_;
+
+            ScorpioInterface* interface_;
+            ScorpioSensorData* sensordata_;
+            ScorpioCommand* command_;
     };
 
     // Register this plugin with the simulator
